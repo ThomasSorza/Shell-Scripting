1f6f66fe27fdd7ac972bc8204a0c4a55823b3bfb
ThomasSorza <thomas@sorza.com>
2022-04-10 10:13:32 -0500

    Added LessThan.sh and LessEqual.sh files, (-lt, -le) OPerators

---
+++ OperatorsEx/IntegerOperators/LessEqual.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+n=50
+if [ $n -le 50 ]
+then
+  echo "The number is less than or equal to 50 "
+else
+  echo "THe number is greater than 50 "
+fi
+#(-le) Less than or equal to Operator
---
+++ OperatorsEx/IntegerOperators/LessThan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+n=50
+if [ $n -lt 50 ]
+then
+  echo "The number is less than 50 "
+else
+  echo "THe number is equal or grater than 50"
+fi
+#(-lt) less Than operator
