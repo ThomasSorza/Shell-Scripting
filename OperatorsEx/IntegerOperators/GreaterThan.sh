7ebd6f1c0d58046b5b785f930e60afccc3a09e62
ThomasSorza <thomas@sorza.com>
2022-04-10 09:24:16 -0500

    Added GreaterThan.sh file, (-gt) operator

---
+++ OperatorsEx/IntegerOperators/GreaterThan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+n=50
+if [ $n -gt 50 ]
+then
+  echo "The number is greater tan 50 "
+else
+  echo "The number is less or equal to 50 "
+fi
+#-gt evaluates if a value is greater than other
