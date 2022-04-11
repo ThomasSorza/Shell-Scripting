6c7a3f78d36f4e1ea12e77241ce3430158f6f273
ThomasSorza <thomas@sorza.com>
2022-04-10 20:39:05 -0500

    Added files LengthZero.sh and NotZero.sh, compare String with zero

---
+++ OperatorsEx/StringOperators/LengthZero.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+str=""
+if [ -z $str ]
+then
+  echo "The String length is zero"
+else
+  echo "The String lenght is more than zero"
+fi
+#Check if the String length is Zero
---
+++ OperatorsEx/StringOperators/NotZero.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+str="Linux"
+if [ -n $str ]
+then
+  echo "The string length is non-zero"
+else
+  echo "The string length is zero"
+fi
