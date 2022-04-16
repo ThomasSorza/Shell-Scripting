03b94279f22979ebdb77b5e59f70f1c41d5facaf
ThomasSorza <thomas@sorza.com>
2022-04-15 23:03:29 -0500

    Created LogicalOperators directory with (-a Operator) file in

---
+++ OperatorsEx/LogicalOperators/AND_Logic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#-------(-a) Operator ----------
+n1=25
+n2=65
+if [ $n1 -gt 24 -a $n2 -lt 66 ]
+then
+  echo "You are elegible"
+else
+  echo "You are not elegible"
+fi
+#-------(-a) OPerator acts like the Logical AND.---------
