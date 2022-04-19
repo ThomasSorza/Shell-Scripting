ea5edec8c5e22c4c473c6209190a04d17fbfad10
ThomasSorza <thomas@sorza.com>
2022-04-19 01:10:11 -0500

    Organized Files in respectives folders, updated terminary.sh file

--- OperatorsEx/terminary.sh
+++ OperatorsEx/terminary.sh
@@ -1,6 +1,12 @@
+#!/bin/bah
+
 n=20
 v1=100
 v2=200
 echo $(( n>=20 ? v1 : v2 ))
+# if the condition before '?' is true, Will execute the statement 
+# defined before ':'. 
+# if not, the statement after the ':' will be executed.
+# (ALTERNATIVE of if statement).
 
-#Will always be v1
+#In this case will always be v1.
