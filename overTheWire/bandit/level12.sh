5fdd39fe928299abb81141f0d4537958d2dc3bf0
Thomas Sorza <sthx09567541@gmail.com>
2023-05-16 21:01:25 -0500

    get second file decompressed

--- overTheWire/bandit/level12.sh
+++ overTheWire/bandit/level12.sh
@@ -2,6 +2,17 @@
 
 xxd -r level12.hex > data.gzip
 
-#decomplessor
-name_compressed=&(7z l data.gzip | grep -A 2 Name | tail  -n 1 | awk NF{'print $NF'} > /dev/null)
-7z x $name_compressed > /dev/null 2>&1
+#decompressor
+name_compressed=$(7z l data.gzip | grep -A 2 Name | tail -n 1 | awk NF{'print $NF'})
+7z x data.gzip > /dev/null 2>&1
+
+while true; do
+    7z l $name_compressed > /dev/null 2>&1
+
+    if [ $(echo $?) == "0" ];then
+      decompressed_next=$(7z l $name_compressed | grep -A 2 Name | tail  -n 1 | awk NF{'print $NF'} > /dev/null)
+      7z x $name_compressed > /dev/null 2>&1 && name_compressed=$decompressed_next
+    else
+        exit 1
+    fi
+done
