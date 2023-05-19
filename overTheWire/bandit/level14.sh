0b24071d2ddb126a27b215e1aefc1a603ac6d6fa
Thomas Sorza <sthx09567541@gmail.com>
2023-05-19 02:21:16 -0500

    edited level 14

--- overTheWire/bandit/level14.sh
+++ overTheWire/bandit/level14.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-nc localhost 30000
-#type the password of the current level
-#Another way to do it (using telnet):
+#check port
+#cat /etc/bandit_pass/bandit14
+#bash -c "echo '' > /dev/tcp/127.0.0.1/30000" 2>/dev/null && echo "Open Port" || echo "Closed Port"
+
+echo $(cat /etc/bandit_pass/bandit14) | nc localhost 30000 | tail-n 2 | head -n 1
+
 #telnet localhost 30000
-#type the password again and press enter
+#enter the password $(cat /etc/bandit_pass/bandit14)
