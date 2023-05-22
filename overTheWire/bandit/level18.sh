d0bbe74bd499b108103bee31b01c3fab763d83f0
Thomas Sorza <sthx09567541@gmail.com>
2023-05-22 13:39:36 -0500

    added level18

---
+++ overTheWire/bandit/level18.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh bandit18@bandit.labs.overthewire.org -p 2220 bash
+#you will get no pront or you can display the pseudo console pront
+#whoami : to confirm connection (bandit 18 must be the output)
+cat readme
