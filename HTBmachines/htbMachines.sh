6856b7304f92d06ac0c254d3a22733e9fd608bd1
Thomas Sorza <sthx09567541@gmail.com>
2023-07-26 01:30:33 -0500

    added bundle.js machine file

--- HTBmachines/htbMachines.sh
+++ HTBmachines/htbMachines.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+#Colors
+greenColor="\e[0;32m\033[1m"
+endColor="\033[0m\e[0m"
+redColor="\e[0;31m\033[1m"
+blueColor="\e[0;34m\033[1m"
+yellowColor="\e[0;33m\033[1m"
+purpleColor="\e[0;35m\033[1m"
+turquoiseColor="\e[0;36m\033[1m"
+grayColor="\e[0;37m\033[1m"
+
 function ctrl_c(){
-  echo -e "\n\n [!] Exiting...\n"
+  echo -e "\n\n ${redColor} [!] Exiting...${endColor}\n"
   exit 1
 }
 
 #Ctrl_c
 trap ctrl_c INT
+
+sleep 10
