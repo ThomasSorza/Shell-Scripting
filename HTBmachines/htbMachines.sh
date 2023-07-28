3ecc46950da326ee3f3f098c134ad8bca61d3a20
Thomas Sorza <sthx09567541@gmail.com>
2023-07-27 20:12:13 -0500

    finished the searchByIp function

--- HTBmachines/htbMachines.sh
+++ HTBmachines/htbMachines.sh
@@ -9,6 +9,8 @@ yellowColor="\e[0;33m\033[1m"
 purpleColor="\e[0;35m\033[1m"
 turquoiseColor="\e[0;36m\033[1m"
 grayColor="\e[0;37m\033[1m"
+color_code="\033[0;31m"   # Red color code
+reset_code="\033[0m"      # Reset color code
 
 #Global Variables
 main_link="https://htbmachines.github.io/bundle.js"
@@ -28,15 +30,14 @@ function helpPanel(){
   echo -e "\t ${turquoiseColor} -m: ${endColor} ${purpleColor}[machine name]${purpleColor} ${grayColor} Search by machine name. ${endColor}\n"
   echo -e "\t ${turquoiseColor} -h: ${endColor} ${grayColor}Display this help panel.${endColor}\n"
   echo -e "\t ${turquoiseColor} -u: ${endColor} ${grayColor}Update the Machine File.${endColor}\n"
-  echo -e "\t ${turquoiseColor} -o: ${endColor} ${purpleColor}[operating_system]${endColour} ${grayColor} Search machines by operating system ${endColor}\n"
-}
-
-function searchMachine(){
-  echo "searching $1"
+  echo -e "\t ${turquoiseColor} -o: ${endColor} ${purpleColor}[operating_system]${endColour} ${grayColor} Search machines by operating system. ${endColor}\n"
+  echo -e "\t ${turquoiseColor} -i: ${endColor} ${purpleColor}[ip_address]${endColour} ${grayColor} Search machines by IP address. ${endColor}\n"
 }
 
 function updateMachineFile(){
   tput civis
+  echo -e "\n${yellowColor}[*]${endColor} ${grayColor} Cheking for Machines Updates... ${endColor}"
+  sleep 1.2
   if [ -f "$bundle" ]; then #Update the machines
     echo -e "\n${greenColor}[*]${endColor} Machines Files Already exist. Checking for updates ..."
     curl -s "$main_link" > "$temp"
@@ -48,8 +49,7 @@ function updateMachineFile(){
       rm $temp
     else
       echo -e "\n${greenColor}[*]${endColor} Updating machines files ..."
-      cat "$temp" > "$bundle"
-      rm $temp
+      rm $bundle && mv "$temp" "$bundle"
       echo -e "\n${greenColor}[*]${endColor} Machines file succesfully updated."
     fi
   else #downloading the machines
@@ -61,6 +61,26 @@ function updateMachineFile(){
   tput cnorm
 }
 
+function searchMachine(){
+  machine_name="$1"
+  if grep -q "$machine_name" $bundle; then #check if machine exist
+    echo -e "\n${greenColor}[!] Listing $machine_name's Machine Properties:${endColor}\n"
+    cat $bundle | awk "/name: \"$machine_name\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta:" | tr -d '"' | tr -d ',' | sed 's/^ *//' | sed 's/:/ -> /' | awk -v color="$color_code" -v reset="$reset_code" '{printf color "%s" reset, $1; for (i=2; i<=NF; i++) printf " %s", $i; print ""}'
+  else
+    echo -e "\n${redColor}[!] ERROR:${endColor} Machine named $machine_name do not exist or is not found.\n"
+  fi
+}
+
+function searchByIp(){
+  ip=$1
+  if grep -q "$ip" $bundle; then
+    machineName=$(grep -B 3 "\"$ip\"" $bundle | grep "name:" | awk NF'{print $NF}' | tr -d '"|,')
+    searchMachine $machineName
+  else
+    echo -e "\n${redColor}[!] ERROR:${endColor} IP address $ip do not exist or is not found.\n"
+  fi
+}
+
 #function searchByOs(){
   #TODO: Search by OS
 #}
@@ -69,10 +89,11 @@ function updateMachineFile(){
 declare -i parameter_counter=0
 
 #TODO add -o option for OS
-while getopts "m:hu" arg; do
+while getopts "m:hi:u" arg; do
   case $arg in
     m) machine_name=$OPTARG; let parameter_counter+=1;;
     h) ;;
+    i) ipAdd=$OPTARG; let parameter_counter+=3;;
     u) parameter_counter+=2;;
     #0) searchByOS;;
   esac
@@ -82,6 +103,8 @@ if [ $parameter_counter -eq 1 ]; then
   searchMachine $machine_name
 elif [ $parameter_counter -eq 2 ]; then
   updateMachineFile
+elif [ $parameter_counter -eq 3 ]; then
+  searchByIp $ipAdd
 else
   helpPanel
 fi
